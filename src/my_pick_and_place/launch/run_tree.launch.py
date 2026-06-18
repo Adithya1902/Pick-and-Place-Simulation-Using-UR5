4b0cd8021687e30a8f804fa6e68055e8dcf92bf2
Adithya1902 <113539806+Adithya1902@users.noreply.github.com>
2026-06-18 09:00:16 +0530

    Update run_tree.launch.py

--- src/my_pick_and_place/launch/run_tree.launch.py
+++ src/my_pick_and_place/launch/run_tree.launch.py
@@ -3,7 +3,6 @@ from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
 
 def generate_launch_description():
-    # 1. Gather the MoveIt configuration parameters
     moveit_config = (
         MoveItConfigsBuilder("my_ur5_gripper_moveit_config", package_name="my_ur5_gripper_moveit_config")
         .robot_description(file_path="config/ur5_robotiq.urdf.xacro")
@@ -12,14 +11,13 @@ def generate_launch_description():
         .to_moveit_configs()
     )
 
-    # 2. Wrap your C++ executable and sync the clocks
     bt_node = Node(
         package="my_pick_and_place",
         executable="pick_and_place_bt_node",
         output="screen",
         parameters=[
             moveit_config.to_dict(),
-            {"use_sim_time": True}  # <-- This tells the Brain to use Gazebo's clock
+            {"use_sim_time": True}  
         ]
     )
 
